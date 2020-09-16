6fab717ea298036b3bdfe6170e4eefafa3ca296d
George Svitenko <georg.svit@gmail.com>
2020-09-16 03:24:09 +0300

    Ordering functional

---
+++ BikeStore/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikeStore.Data;
+using BikeStore.Extensions;
+using BikeStore.Models.Cart;
+using BikeStore.Models.Domain;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Checkout() => View(new OrderHeader());
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(OrderHeader orderHeader)
+        {
+            if (ModelState.IsValid)
+            {
+                orderHeader.CreationDate = DateTime.Now;
+                orderHeader.UserId = _context.Users.FirstOrDefault(u => u.Email == User.Identity.Name).Id;
+                await _context.AddAsync(orderHeader);
+                await _context.SaveChangesAsync();
+
+                List<Item> cart = HttpContext.Session.GetObjectFromJson<List<Item>>("cart");
+
+                foreach (var item in cart)
+                {
+                    for (int i = 0; i < item.Quantity; i++)
+                    {
+                        Bike bike = _context.Bikes.FirstOrDefault(x => x.ModelColour == item.ModelColour && x.FrameSizeId == item.FrameSize.Id && x.StatusId == 1);
+                        bike.StatusId = 2;
+                        _context.Update(bike);
+                        await _context.AddAsync(new OrderDetail() { BikeId = bike.Id, OrderHeaderId = orderHeader.Id });
+                        await _context.SaveChangesAsync();
+                    }
+                }
+                return RedirectToAction("CompletedOrder");
+            }
+            else
+            {
+                return View(orderHeader);
+            }
+        }
+
+        public ViewResult CompletedOrder()
+        {
+            HttpContext.Session.SetObjectAsJson("cart", new List<Item>());
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
